baa0fe81f63580d65fce5afe40445d8019133add
Barry Brown <profbbrown@gmail.com>
2024-03-02 16:10:36 -0800

    Update to use EVP functions

--- md5.c
+++ md5.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <openssl/md5.h>
+#include <stdint.h>
+#include <openssl/evp.h>
 
 char *md5(const char *str, int length) {
-    int n;
-    MD5_CTX c;
-    unsigned char digest[MD5_DIGEST_LENGTH];
-    char *out = (char*)malloc(MD5_DIGEST_LENGTH * 2 + 1);
+    EVP_MD_CTX *mdctx;
+    unsigned int md5_digest_len = EVP_MD_size(EVP_md5());
+    uint8_t md5_digest[md5_digest_len];
+    char *hexdigest = (char*) malloc(md5_digest_len * 2 + 1);
 
-    MD5_Init(&c);
+    // MD5_Init
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
 
+    // MD5_Update
     while (length > 0) {
         if (length > 512) {
-            MD5_Update(&c, str, 512);
+            EVP_DigestUpdate(mdctx, str, 512);
         } else {
-            MD5_Update(&c, str, length);
+            EVP_DigestUpdate(mdctx, str, length);
         }
         length -= 512;
         str += 512;
     }
 
-    MD5_Final(digest, &c);
+    // MD5_Final
+    EVP_DigestFinal_ex(mdctx, md5_digest, &md5_digest_len);
+    EVP_MD_CTX_free(mdctx);
 
-    for (n = 0; n < MD5_DIGEST_LENGTH; ++n) {
-        snprintf(&(out[n*2]), MD5_DIGEST_LENGTH*2, "%02x", (unsigned int)digest[n]);
+    for (int n = 0; n < md5_digest_len; ++n) {
+        snprintf(hexdigest + n*2, 3, "%02x", md5_digest[n]);
     }
 
-    out[MD5_DIGEST_LENGTH * 2] = '\0';
+    hexdigest[md5_digest_len * 2] = '\0';
 
-    return out;
+    return hexdigest;
 }
